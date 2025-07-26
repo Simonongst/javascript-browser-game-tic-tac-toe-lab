185ced693088e0f9b579ccbdc689cdfc8c78fc21
Simon Ong <simonongst@gmail.com>
2025-07-26 09:39:28 +0800

    variable and cached element reference declared, function init() created

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let board = ['', '', '', '', '', '', '', '', '',];
+let turn = 'X';
+let winner = false;
+let tie = false;
+
+
+/*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelector('.sqr');
+const messageEl = document.querySelector('#message');
+
+console.log(squareEls);
+console.log(messageEl);
+
+
+/*-------------------------------- Functions --------------------------------*/
+const init = () => {
+    
+};
+
+init()
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
